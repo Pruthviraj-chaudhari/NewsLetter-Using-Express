9b9f365a6d025174df0512a5a3cabeffbc514fe2
Pruthviraj-chaudhari <chaudharipruthviraj888@gmail.com>
2023-06-09 21:20:27 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  const fname = req.body.firstName;
+  const lname = req.body.lastName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        FNAME: fname,
+        LNAME: lname
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us12.api.mailchimp.com/3.0/lists/bf444f19bf";
+  const options = {
+    method: "POST",
+    auth: "pruthvi:e42115c1da2a674331be81254012d2cd-us12"
+  };
+
+  const apiRequest = https.request(url, options, (response) => {
+
+    if(response.statusCode == 200){
+      res.sendFile(__dirname+"/success.html");  
+    }else{
+      res.sendFile(__dirname+"/failure.html");  
+    }
+
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  apiRequest.write(jsonData);
+  apiRequest.end();
+
+  // res.sendFile(__dirname + "/success.html");
+});
+
+app.post("/failure", (req, res)=>{
+  res.redirect("/");
+})
+
+app.post("/success", (req, res)=>{
+  res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server Running on port 3000");
+});
